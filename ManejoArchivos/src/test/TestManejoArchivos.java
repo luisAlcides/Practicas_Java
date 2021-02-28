f07ec43b17edad8e736aa5baaa1b78c9982e9422
Luis Alcides <luisalcidesblandon@gmail.com>
2021-02-28 15:14:54 -0600

    ApiColeccionesGenericas y Manejo de Archivos

---
+++ ManejoArchivos/src/test/TestManejoArchivos.java
@@ -0,0 +1,19 @@
+package test;
+
+import static manejoarchivo.ManejoArchivos.*;
+
+public class TestManejoArchivos {
+
+    public static void main(String[] args) {
+        var nombreArchivo = "prueba.txt";
+//        crearArchivo(nombreArchivo);
+
+//        escribirArchivo(nombreArchivo, "Hola mundo");
+//        agregarArchivo(nombreArchivo, "Otra linea de codigo");
+
+
+        leerArchivo(nombreArchivo);
+
+        
+    }
+}
